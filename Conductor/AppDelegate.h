d40bccd8da753d3829137ab32b983061db8ac15c
Draco Yam <yamdraco@yahoo.com>
2014-07-08 13:07:35 +0800

    Initial Commit

---
+++ Conductor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Conductor
+//
+//  Created by Draco Yam on 7/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Conductor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Conductor
+//
+//  Created by Draco Yam on 7/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
